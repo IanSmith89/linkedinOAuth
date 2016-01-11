547021d4c9bbc9a017388f90b3695036166c17f9
Ian Smith <iansmith1026@gmail.com>
2016-01-11 13:53:29 -0700

    updated callback

--- app.js
+++ app.js
@@ -38,8 +38,9 @@ passport.deserializeUser(function(user, done) {
 passport.use(new LinkedInStrategy({
     clientID: process.env.LINKEDIN_CLIENT_ID,
     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/linkedin/callback",
+    callbackURL: process.env.HOST + '/auth/linkedin/callback',
     scope: ['r_emailaddress', 'r_basicprofile'],
+    state: true
   },
   function(accessToken, refreshToken, profile, done) {
     done(null, {id: profile.id, displayName: profile.displayName, token: accessToken});
